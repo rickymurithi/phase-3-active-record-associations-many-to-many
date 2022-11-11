e3ce81fcb63bd7181197cb0b8f6f2871c102cf60
erickmuriithi <rickymurithii@gmail.com>
2022-11-11 10:09:40 +0300

    add files

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
   has_many :reviews
   has_many :users, through::reviews
-
+  
 end
--- db/migrate/20221105072021_add_user_id_to_reviews.rb
+++ db/migrate/20221105072021_add_user_id_to_reviews.rb
@@ -1,5 +1,5 @@
 class AddUserIdToReviews < ActiveRecord::Migration[6.1]
-    def change
-      add_column :reviews, :user_id, :integer
-    end
+  def change
+    add_column :reviews, :user_id, :integer
+  end
 end
